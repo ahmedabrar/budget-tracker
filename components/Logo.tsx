1836ac29ca770f362d7e92aec2af7c6c604a0244
Ahmed Abrar <abrahmed@gmail.com>
2024-04-29 16:14:31 +0100

    Initial Layout and Navbar completed

---
+++ components/Logo.tsx
@@ -0,0 +1,25 @@
+import { PiggyBank } from "lucide-react";
+import React from "react";
+
+function Logo() {
+  return (
+    <a href="/" className="flex items-center gap-2">
+      <PiggyBank className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" />
+      <p className="bg-gradient-to-r from-amber-500 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent dark:shadow-2xl dark:shadow-yellow-800">
+        BudgetTracker
+      </p>
+    </a>
+  );
+}
+
+export function LogoMobile() {
+  return (
+    <a href="/" className="flex items-center gap-2">
+      <p className="bg-gradient-to-r from-amber-500 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent dark:shadow-2xl dark:shadow-yellow-800">
+        BudgetTracker
+      </p>
+    </a>
+  );
+}
+
+export default Logo;
