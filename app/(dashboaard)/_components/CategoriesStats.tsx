66268509eacc900cd6c9ae0475b1c43359f729eb
Ahmed Abrar <abrahmed@gmail.com>
2024-05-08 14:28:16 +0100

    complete dashboard categories

--- app/(dashboaard)/_components/CategoriesStats.tsx
+++ app/(dashboaard)/_components/CategoriesStats.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { GetCategoriesStatsResponseType } from "@/app/api/stats/categories/route";
+import SkeletonWrapper from "@/components/SkeletonWrapper";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { DateToUTCDate, GetFormatterForCurrency } from "@/lib/helpers";
+import { TransactionType } from "@/lib/types";
 import { UserSettings } from "@prisma/client";
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useMemo } from "react";
 
 interface Props {
   from: Date;
@@ -10,7 +18,108 @@ interface Props {
 }
 
 function CategoriesStats({ from, to, userSettings }: Props) {
-  return <div>CategoriesStats</div>;
+  const statsQuery = useQuery<GetCategoriesStatsResponseType>({
+    queryKey: ["overview", "stats", "categories", from, to],
+    queryFn: () =>
+      fetch(
+        `/api/stats/categories?from=${DateToUTCDate(from)}&to=${DateToUTCDate(
+          to
+        )}`
+      ).then((res) => res.json()),
+  });
+
+  const formatter = useMemo(() => {
+    return GetFormatterForCurrency(userSettings.currency);
+  }, [userSettings.currency]);
+
+  return (
+    <div className="flex flex-wrap w-full gap-2 md:flex-nowrap">
+      <SkeletonWrapper isLoading={statsQuery.isFetching}>
+        <CategoriesCard
+          type="income"
+          data={statsQuery.data || []}
+          formatter={formatter}
+        />
+      </SkeletonWrapper>
+      <SkeletonWrapper isLoading={statsQuery.isFetching}>
+        <CategoriesCard
+          type="expense"
+          data={statsQuery.data || []}
+          formatter={formatter}
+        />
+      </SkeletonWrapper>
+    </div>
+  );
 }
 
 export default CategoriesStats;
+
+function CategoriesCard({
+  type,
+  data,
+  formatter,
+}: {
+  type: TransactionType;
+  formatter: Intl.NumberFormat;
+  data: GetCategoriesStatsResponseType;
+}) {
+  const filteredData = data.filter((el) => el.type === type);
+
+  const total = filteredData.reduce(
+    (acc, el) => acc + (el._sum?.amount || 0),
+    0
+  );
+  return (
+    <Card className="h-80 w-full col-span-6">
+      <CardHeader>
+        <CardTitle className="grid grid-flow-row justify-between gap-2 text-muted-foreground md:grid-flow-col">
+          {type === "income" ? "Incomes" : "Expenses"} by catergory
+        </CardTitle>
+      </CardHeader>
+      <div className="flex items-center justify-between gap-2">
+        {filteredData.length === 0 && (
+          <div className="flex flex-col w-full h-60 justify-center items-center">
+            No data for the selected period
+            <p className="text-xs text-muted-foreground">
+              Try selecting different period or trying add new{" "}
+              {type === "expense" ? "expensne" : "income"}
+            </p>
+          </div>
+        )}
+
+        {filteredData.length > 0 && (
+          <ScrollArea className="h-60 w-full px-4">
+            <div className="flex w-full flex-col gap-4 p-4">
+              {filteredData.map((item) => {
+                const amount = item._sum.amount || 0;
+                const percentage = (amount * 100) / (total || amount);
+
+                return (
+                  <div className="flex flex-col gap-2" key={item.category}>
+                    <div className="flex items-center justify-between">
+                      <span className="flex items-center text-gray-400">
+                        {item.categoryIcon} {item.category}
+                        <span className="ml-2 text-xs text-muted-foreground">
+                          {percentage.toFixed(0)}%
+                        </span>
+                      </span>
+                      <span className="text-sm text-gray-400">
+                        {formatter.format(amount)}
+                      </span>
+                    </div>
+                    <Progress
+                      value={percentage}
+                      indicator={
+                        type === "income" ? "bg-emerald-500" : "bg-red-500"
+                      }
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </ScrollArea>
+        )}
+      </div>
+    </Card>
+  );
+}
